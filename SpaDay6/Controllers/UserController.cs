204c66a89458e5166109589f3b447ade641e3756
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-03 10:09:18 -0400

    coded solution for ch 14

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6;
+
+[Route("/user")]
+public class UserController : Controller
+{
+    [HttpGet]
+    public IActionResult RenderUserWelcomePage()
+    {
+        return View("Index");
+    }
+
+    [HttpGet("add")]
+    public IActionResult RenderAddUserForm()
+    {
+        return View("Add");
+    }
+
+    [HttpPost("add")]
+    public IActionResult ProcessAddUserForm(string username, string email, string password, string verify)
+    {
+        if (password != verify)
+        {
+            ViewBag.Username = username;
+            ViewBag.Email = email;
+            ViewBag.Error = "Passwords must match.";
+            return View("Add");
+        }
+        ViewBag.User = new User(username, email, password);
+        return View("Index");
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaDay6;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime Date { get; set; } // Bonus mission 1
+
+    public User(string username, string email, string password)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        Date = DateTime.Now; // Bonus mission 1
+    }
+
+
+
+}
